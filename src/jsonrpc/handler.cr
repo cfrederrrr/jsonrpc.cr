7acef4fc5cadf9a210a376e8962842fedb649403
Carl Frederick <galvertez@gmail.com>
2018-05-31 22:51:11 -0400

    slight update to batch processing

--- src/jsonrpc/handler.cr
+++ src/jsonrpc/handler.cr
@@ -14,33 +14,35 @@ class JSONRPC::Handler
   def handle(json : String) : String
     parser = JSON::PullParser.new(json)
 
-    case parser.kind
-    when :begin_array
-      batch = [] of Request(JSON::Any)
-      parser.read_array{ batch << Request(JSON::Any).new(parser) }
-      return handle batch
-    when :begin_object
-      request = Request(JSON::Any).new(parser)
-      return handle request
+    begin
+      case parser.kind
+      when :begin_array
+        batch = [] of Request(JSON::Any)
+        parser.read_array{ batch << Request(JSON::Any).new(parser) }
+        return handle batch
+      when :begin_object
+        request = Request(JSON::Any).new(parser)
+        return handle request
+      else
+        return Response(Nil).new(InvalidRequest.new).to_json
+      end
+    rescue JSON::ParseException
+      return Response(Nil).new(ParseError.new) unless request
     end
-
-    return Response(Nil).new(ParseError.new) unless request
-    return Response(Nil).new(InvalidRequest.new, request.id) unless (
-      request.jsonrpc == RPCVERSION
-    )
 
     handle(request)
   end
 
   def handle(batch : Array(Request(JSON::Any))) : String
     response = [] of String
-    batch.each do |request|
-      response.push handle request
-    end
+    batch.each{ |request| response.push(handle request) }
     "[#{response.join(',')}]"
   end
 
   def handle(request : Request(JSON::Any)) : String
+    unless request.jsonrpc == RPCVERSION
+      return Response(Nil).new(InvalidRequest.new, request.id).to_json
+    end
     m = @methods[request.name]?
     return Response(Nil).new(MethodNotFound.new, request.id).to_json if m.nil?
     result = m.call(request)
