35050aa3c77f8e7197ade4e6bc8efc36dde95b74
galvertez <galvertez@gmail.com>
2018-06-10 12:50:34 -0400

    updates to spec

--- spec/jsonrpc/handler_spec.cr
+++ spec/jsonrpc/handler_spec.cr
@@ -1,9 +1,16 @@
 require "../spec_helper"
 
 describe "JSONRPC::Handler" do
-  describe "#register_method" do
-    it "registers a method" do
+  describe "#register" do
 
+    it "registers a method" do
+      JSONRPC.register "no-op" { nil }
+      JSONRPC.method?("no-op").should be_a(JSONRPC::Method)
     end
+
+    it "handles a request" do
+      response = JSONRPC.handle %<{"jsonrpc":"2.0","method":"no-op","id":9001}>
+    end
+
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,9 +3,9 @@ require "http/server"
 
 require "../src/jsonrpc"
 
-JSONRPC::TEST_SERVER = HTTP::Server.new(8080) do |context|
+TEST_SERVER = HTTP::Server.new(8080) do |context|
   context.response.content_type = "application/json"
   context.response.print JSONRPC.handle(context.request.body)
 end
 
-at_exit { JSONRPC::TEST_SERVER.listen }
+TEST_SERVER.listen
